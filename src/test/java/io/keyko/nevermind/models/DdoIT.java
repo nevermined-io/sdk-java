719134469c53a0d1f4b5f341a604a861aebadaaa
Aitor <1726644+aaitor@users.noreply.github.com>
2020-04-28 11:18:05 +0200

    ddo integration tests now are using public examples, implements #5

--- src/test/java/io/keyko/nevermind/models/DdoIT.java
+++ src/test/java/io/keyko/nevermind/models/DdoIT.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
@@ -22,20 +21,20 @@ public class DdoIT {
 
     private static final Logger log = LogManager.getLogger(DdoIT.class);
 
-    private static final String OEP12_COMPUTING_EXAMPLE_URL = "https://raw.githubusercontent.com/oceanprotocol/OEPs/master/12/ddo.computing.json";
-    private static final String OEP12_WORKFLOW_EXAMPLE_URL = "https://raw.githubusercontent.com/oceanprotocol/OEPs/master/12/ddo.workflow.json";
+    private static final String DDO_COMPUTING_EXAMPLE_URL = "https://github.com/keyko-io/nevermind-docs/raw/master/architecture/specs/examples/compute/v0.1/ddo.computing.json";
+    private static final String DDO_WORKFLOW_EXAMPLE_URL = "https://github.com/keyko-io/nevermind-docs/raw/master/architecture/specs/examples/compute/v0.1/ddo.workflow.json";
 
-    private static final String OEP7_DATASET_EXAMPLE_URL = "https://raw.githubusercontent.com/oceanprotocol/OEPs/master/8/v0.4/ddo-example-access.json";
-    private static final String OEP7_ALGORITHM_EXAMPLE_URL = "https://raw.githubusercontent.com/oceanprotocol/OEPs/master/8/v0.4/ddo-example-algorithm.json";
-    private static final String OEP7_WORKFLOW_EXAMPLE_URL = "https://raw.githubusercontent.com/oceanprotocol/OEPs/master/8/v0.4/ddo-example-workflow.json";
-    private static final String OEP7_SERVICE_EXAMPLE_URL = "https://raw.githubusercontent.com/oceanprotocol/OEPs/master/8/v0.4/ddo-example-service.json";
+    private static final String ACCESS_DATASET_EXAMPLE_URL = "https://github.com/keyko-io/nevermind-docs/raw/master/architecture/specs/examples/metadata/v0.1/ddo-example-access.json";
+    private static final String ACCESS_ALGORITHM_EXAMPLE_URL = "https://github.com/keyko-io/nevermind-docs/raw/master/architecture/specs/examples/metadata/v0.1/ddo-example-algorithm.json";
+    private static final String ACCESS_WORKFLOW_EXAMPLE_URL = "https://github.com/keyko-io/nevermind-docs/raw/master/architecture/specs/examples/metadata/v0.1/ddo-example-workflow.json";
+    private static final String ACCESS_SERVICE_EXAMPLE_URL = "https://github.com/keyko-io/nevermind-docs/raw/master/architecture/specs/examples/metadata/v0.1/ddo-example-service.json";
 
-    private static  String OEP7_DATASET_EXAMPLE_CONTENT;
-    private static  String OEP7_ALGORITHM_EXAMPLE_CONTENT;
-    private static  String OEP12_COMPUTING_EXAMPLE_CONTENT;
-    private static  String OEP12_WORKFLOW_EXAMPLE_CONTENT;
-    private static  String OEP7_WORKFLOW_EXAMPLE_CONTENT;
-    private static  String OEP7_SERVICE_EXAMPLE_CONTENT;
+    private static  String ACCESS_DATASET_EXAMPLE_CONTENT;
+    private static  String ACCESS_ALGORITHM_EXAMPLE_CONTENT;
+    private static  String COMPUTE_COMPUTING_EXAMPLE_CONTENT;
+    private static  String COMPUTE_WORKFLOW_EXAMPLE_CONTENT;
+    private static  String ACCESS_WORKFLOW_EXAMPLE_CONTENT;
+    private static  String ACCESS_SERVICE_EXAMPLE_CONTENT;
 
     private static final Config config = ConfigFactory.load();
 
@@ -45,13 +44,13 @@ public class DdoIT {
     @BeforeClass
     public static void setUp() throws Exception {
 
-        OEP7_DATASET_EXAMPLE_CONTENT = IOUtils.toString(new URI(OEP7_DATASET_EXAMPLE_URL), "utf-8");
-        OEP7_ALGORITHM_EXAMPLE_CONTENT = IOUtils.toString(new URI(OEP7_ALGORITHM_EXAMPLE_URL), "utf-8");
-//        OEP12_COMPUTING_EXAMPLE_CONTENT = IOUtils.toString(new URI(OEP12_COMPUTING_EXAMPLE_URL), "utf-8");
-//        OEP12_WORKFLOW_EXAMPLE_CONTENT = IOUtils.toString(new URI(OEP12_WORKFLOW_EXAMPLE_URL), "utf-8");
+        ACCESS_DATASET_EXAMPLE_CONTENT = IOUtils.toString(new URI(ACCESS_DATASET_EXAMPLE_URL), "utf-8");
+        ACCESS_ALGORITHM_EXAMPLE_CONTENT = IOUtils.toString(new URI(ACCESS_ALGORITHM_EXAMPLE_URL), "utf-8");
+        COMPUTE_COMPUTING_EXAMPLE_CONTENT = IOUtils.toString(new URI(DDO_COMPUTING_EXAMPLE_URL), "utf-8");
+        COMPUTE_WORKFLOW_EXAMPLE_CONTENT = IOUtils.toString(new URI(DDO_WORKFLOW_EXAMPLE_URL), "utf-8");
 
-//        OEP7_WORKFLOW_EXAMPLE_CONTENT = IOUtils.toString(new URI(OEP7_WORKFLOW_EXAMPLE_URL), "utf-8");
-//        OEP7_SERVICE_EXAMPLE_CONTENT = IOUtils.toString(new URI(OEP7_SERVICE_EXAMPLE_URL), "utf-8");
+        ACCESS_WORKFLOW_EXAMPLE_CONTENT = IOUtils.toString(new URI(ACCESS_WORKFLOW_EXAMPLE_URL), "utf-8");
+        ACCESS_SERVICE_EXAMPLE_CONTENT = IOUtils.toString(new URI(ACCESS_SERVICE_EXAMPLE_URL), "utf-8");
 
         credentials = WalletUtils.loadCredentials(
                 config.getString("account.main.password"),
@@ -62,7 +61,7 @@ public class DdoIT {
     public void testDDOServicesOrder() throws Exception {
 
         log.debug("testDDOServicesOrder");
-        DDO ddoFromJson = DDO.fromJSON(new TypeReference<DDO>() {}, OEP7_DATASET_EXAMPLE_CONTENT);
+        DDO ddoFromJson = DDO.fromJSON(new TypeReference<DDO>() {}, ACCESS_DATASET_EXAMPLE_CONTENT);
         DDO ddo= ddoFromJson.integrityBuilder(credentials);
 
         Assert.assertEquals("metadata", ddo.services.get(0).type);
@@ -73,10 +72,10 @@ public class DdoIT {
     }
 
     @Test
-    public void testOEP7DatasetMetadata() throws Exception {
+    public void testAccessDatasetMetadata() throws Exception {
 
-        log.debug("testOEP7DatasetMetadata");
-        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, OEP7_DATASET_EXAMPLE_CONTENT);
+        log.debug("testAccessDatasetMetadata");
+        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, ACCESS_DATASET_EXAMPLE_CONTENT);
         SortedMap<String, String> checksums = ddo.generateChecksums();
         assertEquals(2, checksums.size());
         assertEquals("9ca5f006901cced5d5b02fd3691b12b619d6a083bb3fca39b8c90bd60f194cf9", checksums.get("0"));
@@ -85,33 +84,31 @@ public class DdoIT {
         DID did = DID.builder(ddo.toJson(ddo.proof.checksum));
         log.debug("Did generated from checksums: " + did.did);
         assertEquals(64, did.getHash().length());
-        assertEquals("did:op:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
+        assertEquals("did:nv:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
 
     }
 
     @Test
-    public void testOEP7AlgorithmMetadata() throws Exception {
+    public void testAccessAlgorithmMetadata() throws Exception {
 
-        log.debug("testOEP7AlgorithmMetadata");
-        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, OEP7_ALGORITHM_EXAMPLE_CONTENT);
+        log.debug("testAccessAlgorithmMetadata");
+        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, ACCESS_ALGORITHM_EXAMPLE_CONTENT);
         SortedMap<String, String> checksums = ddo.generateChecksums();
         assertEquals(1, checksums.size());
-        assertEquals("cac1a3df1d0dbbda8ced1f166ea46287536f15edb89705da11714cbeb588e43f", checksums.get("0"));
+        assertEquals("955892c1ed06ce0f0c6dd05b73f134d56028f873cad393b885425143122b635d", checksums.get("0"));
 
         DID did = DID.builder(ddo.toJson(ddo.proof.checksum));
         log.debug("Did generated from checksums: " + did.did);
         assertEquals(64, did.getHash().length());
-        assertEquals("did:op:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
+        assertEquals("did:nv:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
 
     }
 
-    // Test ignored because services were removed of OEP
-    @Ignore
     @Test
-    public void testOEP12Computing() throws Exception {
+    public void testDDOComputingMetadata() throws Exception {
 
-        log.debug("TestOEP12Computing");
-        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, OEP12_COMPUTING_EXAMPLE_CONTENT);
+        log.debug("testDDOComputingMetadata");
+        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, COMPUTE_COMPUTING_EXAMPLE_CONTENT);
         SortedMap<String, String> checksums = ddo.generateChecksums();
         assertEquals(2, checksums.size());
         assertEquals("574b45e304c190c293855548f19b02ad0c826aee4b76fb3b3c25b27d8317efd7", checksums.get("0"));
@@ -120,66 +117,59 @@ public class DdoIT {
         DID did = DID.builder(ddo.toJson(ddo.proof.checksum));
         log.debug("Did generated from checksums: " + did.did);
         assertEquals(64, did.getHash().length());
-        assertEquals("did:op:3ea445410f608e6453cdcb7dbe42d57a89aca018993d7e87da85993cbccc6308", did.did);
+        assertEquals("did:nv:3ea445410f608e6453cdcb7dbe42d57a89aca018993d7e87da85993cbccc6308", did.did);
 
     }
 
-    // Test ignored because services were removed of OEP
-    @Ignore
     @Test
-    public void testOEP12Workflow() throws Exception {
+    public void testComputeWorkflowMetadata() throws Exception {
 
-        log.debug("TestOEP12Workflow");
-        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, OEP12_WORKFLOW_EXAMPLE_CONTENT);
+        log.debug("testComputeWorkflowMetadata");
+        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, COMPUTE_WORKFLOW_EXAMPLE_CONTENT);
         SortedMap<String, String> checksums = ddo.generateChecksums();
         assertEquals(1, checksums.size());
-        assertEquals("f548c344c09c91883a85207f8eaadde4b448d03fb89208ee7137a73b3894a682", checksums.get("0"));
+        assertEquals("4c17580e13c74d5244dfe5bc574595978e43d9a544e8dd9287707d01a4a9687c", checksums.get("0"));
 
         DID did = DID.builder(ddo.toJson(ddo.proof.checksum));
         log.debug("Did generated from checksums: " + did.did);
         assertEquals(64, did.getHash().length());
-        assertEquals("did:op:3ea445410f608e6453cdcb7dbe42d57a89aca018993d7e87da85993cbccc6308", did.did);
+        assertEquals("did:nv:3ea445410f608e6453cdcb7dbe42d57a89aca018993d7e87da85993cbccc6308", did.did);
 
     }
 
-    // Test ignored because workflows were removed of OEP
-    @Ignore
-    @Test
-    public void testOEP7WorkflowMetadata() throws Exception {
 
-        log.debug("testOEP7WorkflowMetadata");
-        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, OEP7_WORKFLOW_EXAMPLE_CONTENT);
+    @Test
+    public void testAccessWorkflowMetadata() throws Exception {
+
+        log.debug("testAccessWorkflowMetadata");
+        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, ACCESS_WORKFLOW_EXAMPLE_CONTENT);
         SortedMap<String, String> checksums = ddo.generateChecksums();
         assertEquals(1, checksums.size());
-        assertEquals("58649304d489f460bd71c675e177dcab2b78692dc9b329cb1c0c27bb1735eaa5", checksums.get("0"));
+        assertEquals("3c52cc15f444afcdace3643358212bb3b5dcc2bcda7bd16fe5624c75f1509c98", checksums.get("0"));
 
         DID did = DID.builder(ddo.toJson(ddo.proof.checksum));
         log.debug("Did generated from checksums: " + did.did);
         assertEquals(64, did.getHash().length());
-        assertEquals("did:op:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
+        assertEquals("did:nv:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
 
     }
 
-    // Test ignored because services were removed of OEP
-    @Ignore
     @Test
-    public void testOEP7ServiceMetadata() throws Exception {
+    public void testAccessServiceMetadata() throws Exception {
 
-        log.debug("testOEP7ServiceMetadata");
-        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, OEP7_SERVICE_EXAMPLE_CONTENT);
+        log.debug("testAccessServiceMetadata");
+        DDO ddo = DDO.fromJSON(new TypeReference<DDO>() {}, ACCESS_SERVICE_EXAMPLE_CONTENT);
         SortedMap<String, String> checksums = ddo.generateChecksums();
         assertEquals(2, checksums.size());
-        assertEquals("52eb261dc24117d78ffdd2c5b3409f94d013601f1a45aa43da6d1611671c74a3", checksums.get("0"));
-        assertEquals("f12b645393c7640da2cbb172ba27206d9a0c610890f118b1c4bc98c506703e27", checksums.get("1"));
+        assertEquals("a80a2e50a3ad5c50a6cb111cf34cab806ebc2440eb13fe50744ecbee52c13807", checksums.get("0"));
+        assertEquals("4294cb191438237940fe389fee1dcb5a0806432ac92622d50375a01d3406fa62", checksums.get("1"));
 
         DID did = DID.builder(ddo.toJson(ddo.proof.checksum));
         log.debug("Did generated from checksums: " + did.did);
         assertEquals(64, did.getHash().length());
-        assertEquals("did:op:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
+        assertEquals("did:nv:138fccf336883ae6312c9b8b375745a90be369454080e90985fb3e314ab0df25", did.did);
 
     }
-
-
 
 
 }
